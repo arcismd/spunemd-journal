e6d44fb6d1063489e8814b185218c501f207a195
Artur <a.sincarenco.97@gmail.com>
2022-09-23 16:47:08 +0300

    feat: add post component

---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './post.scss'
+
+export const Post = () => {
+  return (
+    <>
+        <div className='post-wrapper'>
+            <div className='post'>
+
+            </div>
+        </div>
+    </>
+  )
+}
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navbar } from '../../components/Navbar/Navbar'
+import { Post} from '../../components/Post/Post'
 
 export const HomePage = () => {
   return (
     <>
         <Navbar />
+        <Post />
     </>
   )
 }
